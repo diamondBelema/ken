067f32cb64ac6739eaf73fc95a57a0a038df934c
diamondBelema <diamondbelema78@gmail.com>
2026-07-20 02:23:08 +0100

    fix: Windows file lock using atomic create instead of syscall.LockFile

--- internal/registry/lock_windows.go
+++ internal/registry/lock_windows.go
@@ -3,8 +3,9 @@
 package registry
 
 import (
+	"fmt"
 	"os"
-	"syscall"
+	"time"
 )
 
 func withStateLock(fn func() error) error {
@@ -14,17 +15,16 @@ func withStateLock(fn func() error) error {
 	}
 	lockPath := statePath + ".lock"
 
-	f, err := os.OpenFile(lockPath, os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-		return err
+	for i := 0; i < 50; i++ {
+		f, err := os.OpenFile(lockPath, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0644)
+		if err == nil {
+			f.WriteString(fmt.Sprintf("%d", os.Getpid()))
+			f.Close()
+			defer os.Remove(lockPath)
+			return fn()
+		}
+		time.Sleep(100 * time.Millisecond)
 	}
-	defer f.Close()
 
-	h := syscall.Handle(f.Fd())
-	if err := syscall.LockFile(h, 0, 0, 1, 0); err != nil {
-		return err
-	}
-	defer syscall.UnlockFile(h, 0, 0, 1, 0)
-
-	return fn()
+	return fmt.Errorf("timeout waiting for lock on %s", lockPath)
 }
